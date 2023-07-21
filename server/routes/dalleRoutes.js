864db950119c74fd48ee577ab5d025721467d7cf
Pepe <pepesainz2593@gmail.com>
2023-07-21 12:11:48 +0200

    first commit

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+import express from "express";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+import cors from "cors";
+
+dotenv.config();
+
+const router = express.Router();
+router.use(cors());
+
+const configuration = new Configuration({
+  organization: "org-3Bg8JC9krqaOAaLF0EkPfUmq",
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL-E!" });
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = aiResponse.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send(error?.response.data.error.message || "Something went wrong");
+  }
+});
+
+export default router;
